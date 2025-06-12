76856a1ca7967180299d73892b5cf2cb231901ab
paulo.nazareth <phnazareth@hotmail.com>
2025-06-11 23:42:54 -0300

    Curso de Angular - 38# Criando um Serviço (Service)

---
+++ aula_37_a_40/servicos/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursosService } from './cursos/cursos.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CursosComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
