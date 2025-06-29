c8875f8c321efcc71a73cd5708b146b773bc5daf
paulo.nazareth <phnazareth@hotmail.com>
2025-06-29 12:29:23 -0300

    Curso de Angular - 55# Rotas Imperatvas: Redirecionamento Via Código

--- aula_50_a_70/rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ aula_50_a_70/rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,6 @@
+import { CursosService } from './../cursos/cursos.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -9,11 +10,17 @@ import { Subscription } from 'rxjs';
 })
 export class CursoDetalheComponent implements OnInit {
 
-  id: string;
+  id: number;
 
   inscricao: Subscription;
 
-  constructor(private route: ActivatedRoute) { 
+  curso: any;
+
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router,
+    private cursoService: CursosService
+  ) { 
     //console.log(this.route);    
     //this.id = this.route.snapshot.params["id"];
   }
@@ -22,6 +29,10 @@ export class CursoDetalheComponent implements OnInit {
     this.inscricao = this.route.params.subscribe(
       (params: any) => {
         this.id = params['id'];
+        this.curso = this.cursoService.getCurso(this.id);
+        if(this.curso == null) {
+          this.router.navigate(['/nao-encontrado']);
+        }
       }
     );
   }
