999ba04cf259c96f188818f038dba03371c828d9
paulo.nazareth <phnazareth@hotmail.com>
2025-07-12 00:01:03 -0300

    Curso de Angular - 73# Formulários - Criando o Projeto Inicial com Bootstrap 3

---
+++ forms/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DataFormComponent } from './data-form/data-form.component';
+
+const routes: Routes = [
+  {
+    path: 'templateForm',
+    component: TemplateFormComponent
+  },
+  {
+    path: 'dataForm',
+    component: DataFormComponent
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'templateForm' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
