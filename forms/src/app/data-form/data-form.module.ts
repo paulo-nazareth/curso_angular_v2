fb23dd9f57ac260e28acf9d2274fc38fb97b502c
paulo.nazareth <phnazareth@hotmail.com>
2025-07-19 22:50:06 -0300

    Curso de Angular - 92# Formulário Reativos: Fazendo Submit

--- forms/src/app/data-form/data-form.module.ts
+++ forms/src/app/data-form/data-form.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DataFormComponent } from './data-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'app/shared/shared.module';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule, 
-    SharedModule
+    SharedModule,
+    HttpModule
   ],
   declarations: [ 
     DataFormComponent 
