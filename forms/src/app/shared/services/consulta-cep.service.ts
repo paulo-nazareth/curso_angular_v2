51288cff8385c622dfb2d260e41d18b48f6d15ab
paulo.nazareth <phnazareth@hotmail.com>
2025-07-20 23:28:42 -0300

    Curso de Angular - 101# Formulário: Serviço de Consulta CEP + ProvideIn

--- forms/src/app/shared/services/consulta-cep.service.ts
+++ forms/src/app/shared/services/consulta-cep.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +21,7 @@ export class ConsultaCepService {
         return this.http.get(`https://viacep.com.br/ws/${cep}/json`);
       }
     }
+    return of({});
   }
+
 }
